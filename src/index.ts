16714cf0cc337ae884ddf60a0ccce0298ef8cf02
Albert Mink <albert.mink@sap.com>
2023-12-14 13:48:55 +0100

    set initially to inline INTF, react on object type

--- src/index.ts
+++ src/index.ts
@@ -31,64 +31,74 @@ initSqlJs().then(() => {
 const reg = new abaplint.Registry();
 abapMonaco.registerABAP(reg);
 
-const filename = "file:///zif_aff_intf_v1.intf.abap";
-const model1 = monaco.editor.createModel(
-  `INTERFACE zif_aff_intf_v1 PUBLIC.
+const initialValue =
+`INTERFACE zif_aff_intf_v1 PUBLIC.
 
+"! <p class="shorttext">Interface Category</p>
+"! Interface category
+"! $values {@link zif_aff_intf_v1.data:co_category}
+TYPES ty_category TYPE n LENGTH 2.
+
+CONSTANTS:
   "! <p class="shorttext">Interface Category</p>
   "! Interface category
-  "! $values {@link zif_aff_intf_v1.data:co_category}
-  TYPES ty_category TYPE n LENGTH 2.
+  BEGIN OF co_category,
+    "! <p class="shorttext">General</p>
+    "! General interface
+    general                      TYPE ty_category VALUE '00',
+    "! <p class="shorttext">Classic BAdI</p>
+    "! Interface definition of a classic BAdI
+    classic_badi                 TYPE ty_category VALUE '01',
+    "! <p class="shorttext">Business (Static Components)</p>
+    "! Business interface for static components
+    business_static_components   TYPE ty_category VALUE '51',
+    "! <p class="shorttext">Business (Instance-dep. components)</p>
+    "! Business interface for instance-dependent components
+    business_instance_components TYPE ty_category VALUE '52',
+    "! <p class="shorttext">DB Procedure Proxy</p>
+    "! Generated interface of a database procedure proxy
+    db_procedure_proxy           TYPE ty_category VALUE '65',
+    "! <p class="shorttext">Web Dynpro Runtime</p>
+    "! Web Dynpro runtime interface
+    web_dynpro_runtime           TYPE ty_category VALUE '80',
+    "! <p class="shorttext">Enterprise Services</p>
+    "! Generated interface of enterprise services
+    enterprise_service           TYPE ty_category VALUE '90',
+  END OF co_category.
 
-  CONSTANTS:
+TYPES:
+  "! <p class="shorttext">Interface Properties</p>
+  "! Interface properties
+  BEGIN OF ty_main,
+    "! $required
+    format_version TYPE zif_aff_types_v1=>ty_format_version,
+    "! <p class="shorttext">Header</p>
+    "! Header
+    "! $required
+    header         TYPE zif_aff_types_v1=>ty_header_60_src,
     "! <p class="shorttext">Interface Category</p>
     "! Interface category
-    BEGIN OF co_category,
-      "! <p class="shorttext">General</p>
-      "! General interface
-      general                      TYPE ty_category VALUE '00',
-      "! <p class="shorttext">Classic BAdI</p>
-      "! Interface definition of a classic BAdI
-      classic_badi                 TYPE ty_category VALUE '01',
-      "! <p class="shorttext">Business (Static Components)</p>
-      "! Business interface for static components
-      business_static_components   TYPE ty_category VALUE '51',
-      "! <p class="shorttext">Business (Instance-dep. components)</p>
-      "! Business interface for instance-dependent components
-      business_instance_components TYPE ty_category VALUE '52',
-      "! <p class="shorttext">DB Procedure Proxy</p>
-      "! Generated interface of a database procedure proxy
-      db_procedure_proxy           TYPE ty_category VALUE '65',
-      "! <p class="shorttext">Web Dynpro Runtime</p>
-      "! Web Dynpro runtime interface
-      web_dynpro_runtime           TYPE ty_category VALUE '80',
-      "! <p class="shorttext">Enterprise Services</p>
-      "! Generated interface of enterprise services
-      enterprise_service           TYPE ty_category VALUE '90',
-    END OF co_category.
+    category       TYPE ty_category,
+    "! <p class="shorttext">Proxy Interface</p>
+    "! Interface is a proxy interface
+    proxy          TYPE abap_bool,
+    "! <p class="shorttext">Descriptions</p>
+    "! Descriptions maintained in SE80
+    descriptions   TYPE zif_aff_oo_types_v1=>ty_descriptions,
+  END OF ty_main.
 
-  TYPES:
-    "! <p class="shorttext">Interface Properties</p>
-    "! Interface properties
-    BEGIN OF ty_main,
-      "! $required
-      format_version TYPE zif_aff_types_v1=>ty_format_version,
-      "! <p class="shorttext">Header</p>
-      "! Header
-      "! $required
-      header         TYPE zif_aff_types_v1=>ty_header_60_src,
-      "! <p class="shorttext">Interface Category</p>
-      "! Interface category
-      category       TYPE ty_category,
-      "! <p class="shorttext">Proxy Interface</p>
-      "! Interface is a proxy interface
-      proxy          TYPE abap_bool,
-      "! <p class="shorttext">Descriptions</p>
-      "! Descriptions maintained in SE80
-      descriptions   TYPE zif_aff_oo_types_v1=>ty_descriptions,
-    END OF ty_main.
+ENDINTERFACE.`;
 
-ENDINTERFACE.`,
+let pattern = /INTERFACE (\w+) PUBLIC./m;
+let abapObj = initialValue.match(pattern)?.[1];
+let abapType = abapObj.replace("zif_aff_", "").substring(0, 4).toUpperCase();
+
+const filename = "file:///"+abapObj;
+
+
+
+const model1 = monaco.editor.createModel(
+  initialValue,
   "abap",
   monaco.Uri.parse(filename),
 );
@@ -160,13 +170,13 @@ async function abapChanged() {
   abap.Classes["ZCL_AFF_ABAP_DOC_READER"].cache.value = [];
 
   // @ts-ignore
-  await abap.context.databaseConnections["DEFAULT"].execute(`UPDATE reposrc SET data = '${escape(contents)}' WHERE progname = 'ZIF_AFF_INTF_V1'`);
+  await abap.context.databaseConnections["DEFAULT"].execute(`UPDATE reposrc SET data = '${escape(contents)}' WHERE progname = '${abapObj.toUpperCase()}'`);
   console.dir("database updated changed");
 
   try {
     // @ts-ignore
-    const result = await abap.Classes["CL_RUN"].run({object_type: new abap.types.String().set("INTF")});
-    editor2.setValue(result.get() || "");
+    const result = await abap.Classes["CL_RUN"].run({object_type: new abap.types.String().set(abapType)});
+    editor2.setValue(result.get()  || "");
   } catch (error) {
     editor2.setValue(error.message);
     console.dir(error);
